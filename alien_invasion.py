81793f0af60d7aa8d94c2f966894f49723ee7e7a
Vijay Kumar Singh <vjsingh2793@gmail.com>
2024-07-28 14:55:57 +0530

    Right step movement when click right down repeatedly

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,16 +27,26 @@ class AlienInvasion:
     def run_game(self):
         """ Start the main loop for the game. """
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
-            self.screen.fill(self.bg_color)
-            self.ship.blitme()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen. """
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events. """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
