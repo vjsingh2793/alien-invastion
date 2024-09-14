40967c10133316b3f97140e1991ec170f27e40af
Vijay Kumar Singh <vjsingh2793@gmail.com>
2024-09-14 12:34:14 +0530

    Added left and right screen constraints for ship and refactored to add ship speed/movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
         """ Start the main loop for the game. """
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -46,7 +47,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
--- ship.py
+++ ship.py
@@ -14,7 +14,24 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Movement flags; start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left  = False
+
+        # Adjusting the ship speed
+        self.ship_speed = 1.5
     
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.ship_speed
+        # Update rect object from self.x
+        self.rect.x = self.rect.x
